a2bc56d5a6b937348015b947717a2e4c733b4389
Jesugnz <131477030+Jesugnz@users.noreply.github.com>
2024-05-31 12:21:37 +0200

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -61,11 +62,17 @@ android {
 dependencies {
     val nav_version = "2.5.3"
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("com.github.tehras:charts:0.2.4-alpha")
     implementation("com.google.firebase:firebase-analytics")
+    implementation("io.coil-kt:coil-compose:2.1.0")
+    implementation("io.coil-kt:coil-gif:2.1.0")
     implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("com.google.firebase:firebase-firestore:24.11.0")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
